e220b57255209f01b23110191d2daf7cd8eed16a
Sutej Govind Kulkarni <sutejk@Sutejs-MacBook-Pro.local>
2020-03-11 14:02:57 -0700

    NAS GUI changes

---
+++ NAS.py
@@ -0,0 +1,22 @@
+import subprocess
+from pathlib import Path
+LEDGER_PATH = "ledger.json"
+
+# function to open the NAS GUI to enable the user to connect to the network
+def open_NAS_GUI():
+        subprocess.call("python3 NAS_GUI.py",shell=True)
+
+# function to open the file browser
+def open_fileBrowser():
+        subprocess.call("python3 fileBrowser.py",shell=True)
+
+my_file = Path(LEDGER_PATH)
+
+# If ledger exists on your system, that means you are connected to the network,
+# will directly open the file browser.
+# If ledger does not exist on your system, NAS GUI will open and help you get 
+# connected to the network.
+if my_file.is_file():
+	open_NAS_GUI()
+else:
+	open_fileBrowser()
--- NAS_GUI.py
+++ NAS_GUI.py
@@ -3,10 +3,16 @@ from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
         QDial, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
         QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy,
         QSlider, QSpinBox, QStyleFactory, QTableWidget, QTabWidget, QTextEdit,
-        QVBoxLayout, QWidget)
+        QVBoxLayout, QWidget, QLabel)
+#from PyQt5.QTGui import QPixmap
+import PyQt5.QtWidgets
 import sys 
 import os
 import subprocess
+#import client as client_functions
+#import encryption
+#import helperFunctions as helper
+#import ledgerFunctions as ledger
 
 class WidgetGallery(QDialog):
     def __init__(self, parent=None):
@@ -17,207 +23,122 @@ class WidgetGallery(QDialog):
         styleComboBox = QComboBox()
         styleComboBox.addItems(QStyleFactory.keys())
 
-        styleLabel = QLabel("&Style:")
-        styleLabel.setBuddy(styleComboBox)
-
-        self.useStylePaletteCheckBox = QCheckBox("&Use style's standard palette")
-        self.useStylePaletteCheckBox.setChecked(True)
-
-        disableWidgetsCheckBox = QCheckBox("&Disable widgets")
-
-        self.createTopLeftGroupBox()
+        #self.createTopLeftGroupBox()
         self.createTopRightGroupBox()
-        self.createBottomLeftTabWidget()
+        #self.createBottomLeftTabWidget()
         self.createBottomRightGroupBox()
-       #self.createProgressBar()
-        #self.exit_GUI()
 
         styleComboBox.activated[str].connect(self.changeStyle)
-        self.useStylePaletteCheckBox.toggled.connect(self.changePalette)
-        disableWidgetsCheckBox.toggled.connect(self.topLeftGroupBox.setDisabled)
-        disableWidgetsCheckBox.toggled.connect(self.topRightGroupBox.setDisabled)
-        disableWidgetsCheckBox.toggled.connect(self.bottomLeftTabWidget.setDisabled)
-        disableWidgetsCheckBox.toggled.connect(self.bottomRightGroupBox.setDisabled)
 
         topLayout = QHBoxLayout()
-        topLayout.addWidget(styleLabel)
-        topLayout.addWidget(styleComboBox)
+        #topLayout.addWidget(styleLabel)
+        #topLayout.addWidget(styleComboBox)
         topLayout.addStretch(1)
-        topLayout.addWidget(self.useStylePaletteCheckBox)
-        topLayout.addWidget(disableWidgetsCheckBox)
+        #topLayout.addWidget(self.useStylePaletteCheckBox)
 
         mainLayout = QGridLayout()
         mainLayout.addLayout(topLayout, 0, 0, 1, 2)
-        mainLayout.addWidget(self.topLeftGroupBox, 2, 1)
+        #mainLayout.addWidget(self.topLeftGroupBox, 2, 1)
         mainLayout.addWidget(self.topRightGroupBox, 1, 1)
-        mainLayout.addWidget(self.bottomLeftTabWidget, 2, 0)
+        #mainLayout.addWidget(self.bottomLeftTabWidget, 2, 0)
         mainLayout.addWidget(self.bottomRightGroupBox, 1, 0)
        #mainLayout.addWidget(self.progressBar, 3, 0, 1, 2)
-        mainLayout.setRowStretch(1, 1)
+        mainLayout.setRowStretch(5, 1)
         mainLayout.setRowStretch(2, 1)
         mainLayout.setColumnStretch(0, 1)
         mainLayout.setColumnStretch(1, 1)
         self.setLayout(mainLayout)
 
         self.setWindowTitle("Welcome to NAS")
-        self.changeStyle('Windows')
+        self.changeStyle('Macintosh')
 
     def changeStyle(self, styleName):
         QApplication.setStyle(QStyleFactory.create(styleName))
         self.changePalette()
 
     def changePalette(self):
-        if (self.useStylePaletteCheckBox.isChecked()):
-            QApplication.setPalette(QApplication.style().standardPalette())
-        else:
-            QApplication.setPalette(self.originalPalette)
+        QApplication.setPalette(self.originalPalette)
 
-    def advanceProgressBar(self):
-        curVal = self.progressBar.value()
-        maxVal = self.progressBar.maximum()
-        self.progressBar.setValue(curVal + (maxVal - curVal) / 100)
 
-    def createTopLeftGroupBox(self):
-        self.topLeftGroupBox = QGroupBox("Files")
+    def createBottomRightGroupBox(self):
+        self.bottomRightGroupBox = QGroupBox("Enter Server IP address and Public key:")
 
-        radioButton1 = QRadioButton("File 1")
-        radioButton2 = QRadioButton("File 2")
-        radioButton3 = QRadioButton("File 3")
-        radioButton1.setChecked(True)
-        
-        #check for ip entry
-        lineEdit = QLineEdit('s3cRe7')
-        lineEdit.setEchoMode(QLineEdit.Password)
-        
+        self.ip_input = QLineEdit('')
+        self.ip_input.setEchoMode(QLineEdit.Normal)
+
+        self.server_public_key_input = QLineEdit('')
+        self.server_public_key_input.setEchoMode(QLineEdit.Normal)
+
+        flatPushButton = QPushButton("Thank you for choosing NAS!")
+        flatPushButton.setFlat(True)
+
         layout = QGridLayout()
-        layout.addWidget(lineEdit, 0, 0, 1, 2)
-        #check
+        layout.addWidget(self.ip_input, 0, 0, 1, 2)
+        layout.addWidget(self.server_public_key_input, 2, 0, 1, 2)
+        layout.addWidget(flatPushButton, 4, 0, 1, 2)
+        layout.setRowStretch(5, 1)
+        self.bottomRightGroupBox.setLayout(layout)
 
-        checkBox = QCheckBox("Tri-state check box")
-        checkBox.setTristate(True)
-        checkBox.setCheckState(Qt.PartiallyChecked)
-
-        layout = QVBoxLayout()
-        
-        layout.addWidget(radioButton1)
-        layout.addWidget(radioButton2)
-        layout.addWidget(radioButton3)
-        #layout.addWidget(checkBox)
-        layout.addStretch(1)
-        self.topLeftGroupBox.setLayout(layout)
-        
     #function for exiting
     def exit_GUI(self):
         print("GUI closed")
         sys.exit()
-    #
     
     #function for calling another python script
-    
     def call_client(self):
-        subprocess.call("python3 client.py",shell=True)
-        
-   #function for creating top right box(submit and quit buttons)
+        subprocess.call("ls",shell=True)
+
+    # function to open the file browser
+    def open_fileBrowser():
+        subprocess.call("python3 fileBrowser.py",shell=True)
+
+    # Enables the user to connect to the network, takes inputs from ip_input and server_public_key_input
+    def join_the_network(self):
+            client_functions.pull_legder(self.ip_input.text(),self.server_public_key_input.text())
+            open_fileBrowser()
+    
+    # Enables the user to create and start the NAS network
+    def start_my_network(self):
+            client_functions.start_network()
+            open_fileBrowser()
+
+    #Just a check to see if text input gets printed when we click join the network
+    def check_text(self):
+        y = self.ip_input.text()
+        print(y)
+        print(self.server_public_key_input.text())
+
+        #function for creating top right box(submit and quit buttons)
     def createTopRightGroupBox(self):
-        self.topRightGroupBox = QGroupBox("Submission section")
+        self.topRightGroupBox = QGroupBox("NAS network options")
+        
+        #When a user clicks this button "join_the_network" function is called
+        join_nas_network_button = QPushButton("Join the network")
+        join_nas_network_button.clicked.connect(self.join_the_network)
+        
+        #When a user clicks this button, GUI closes
+        quit_button = QPushButton("Quit")
+        quit_button.clicked.connect(self.exit_GUI)
 
-        defaultPushButton = QPushButton("Submit")
-        defaultPushButton.clicked.connect(self.call_client)
-        #defaultPushButton.setDefault(True)
-
-        togglePushButton = QPushButton("Quit")
-        togglePushButton.clicked.connect(self.exit_GUI)
-        #togglePushButton.setDefault(True)
-        #togglePushButton.setCheckable(True)
-        #togglePushButton.setChecked(True)
-
-        flatPushButton = QPushButton("NAS")
-        flatPushButton.setFlat(True)
+        # When a user clicks this button, "start_my_network" function is called
+        create_nas_network_button = QPushButton("Create my network")
+        create_nas_network_button.clicked.connect(self.start_my_network)
 
         layout = QVBoxLayout()
-        layout.addWidget(defaultPushButton)
-        layout.addWidget(togglePushButton)
-        layout.addWidget(flatPushButton)
+        layout.addWidget(join_nas_network_button)
+        layout.addWidget(quit_button)
+        layout.addWidget(create_nas_network_button)
         layout.addStretch(1)
         self.topRightGroupBox.setLayout(layout)
-
-    def createBottomLeftTabWidget(self):
-        self.bottomLeftTabWidget = QTabWidget()
-        self.bottomLeftTabWidget.setSizePolicy(QSizePolicy.Preferred,
-                QSizePolicy.Ignored)
-
-       # tab1 = QWidget()
-       # tableWidget = QTableWidget(10, 10)
-
-        #tab1hbox = QHBoxLayout()
-        #tab1hbox.setContentsMargins(5, 5, 5, 5)
-        #tab1hbox.addWidget(tableWidget)
-        #tab1.setLayout(tab1hbox)
-
-        tab2 = QWidget()
-        #textEdit = QTextEdit()
-        textEdit = QLabel()
-
-       #textEdit.setText("Welcome to the world of NAS")
-        textEdit.setText("We aim to provide a peer-to-peer data sharing platform\n ,NAS, which promises transparent management of distributed\n, fragmented and replicated data on a local network\n as well as effective space utilization on different\n systems present on the network")
-
-        tab2hbox = QHBoxLayout()
-        tab2hbox.setContentsMargins(5, 5, 5, 5)
-        tab2hbox.addWidget(textEdit)
-        tab2.setLayout(tab2hbox)
-
-        #self.bottomLeftTabWidget.addTab(tab1, "&Table")
-        self.bottomLeftTabWidget.addTab(tab2, "About NAS")
-
-    def createBottomRightGroupBox(self):
-        self.bottomRightGroupBox = QGroupBox("Enter Server IP address")
-        self.bottomRightGroupBox.setCheckable(True)
-        self.bottomRightGroupBox.setChecked(True)
-
-        lineEdit = QLineEdit('')
-        lineEdit.setEchoMode(QLineEdit.Normal)
-
-        #spinBox = QSpinBox(self.bottomRightGroupBox)
-        #spinBox.setValue(50)
-
-        dateTimeEdit = QDateTimeEdit(self.bottomRightGroupBox)
-        dateTimeEdit.setDateTime(QDateTime.currentDateTime())
-
-        #slider = QSlider(Qt.Horizontal, self.bottomRightGroupBox)
-        #slider.setValue(40)
-
-        #scrollBar = QScrollBar(Qt.Horizontal, self.bottomRightGroupBox)
-        #scrollBar.setValue(60)
-
-        #dial = QDial(self.bottomRightGroupBox)
-        #dial.setValue(30)
-        #dial.setNotchesVisible(True)
-
-        layout = QGridLayout()
-        layout.addWidget(lineEdit, 0, 0, 1, 2)
-        #layout.addWidget(spinBox, 1, 0, 1, 2)
-        layout.addWidget(dateTimeEdit, 2, 0, 1, 2)
-        #layout.addWidget(slider, 3, 0)
-        #layout.addWidget(scrollBar, 4, 0)
-        #layout.addWidget(dial, 3, 1, 2, 1)
-        layout.setRowStretch(5, 1)
-        self.bottomRightGroupBox.setLayout(layout)
-
-   # def createProgressBar(self):
-   #     self.progressBar = QProgressBar()
-   #     self.progressBar.setRange(0, 10000)
-   #     self.progressBar.setValue(0)
-
-    #    timer = QTimer(self)
-   #     timer.timeout.connect(self.advanceProgressBar)
-   #     timer.start(1000)
 
 
 if __name__ == '__main__':
 
     import sys
 
-    app = QApplication(sys.argv)
+    #app = QApplication(sys.argv)
+    app = QApplication([])
+    print(PyQt5.QtWidgets.QStyleFactory.keys())
     gallery = WidgetGallery()
     gallery.show()
     sys.exit(app.exec_())
